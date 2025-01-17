7311837da6c0a1fed0166618da8ec9389eab734f
JulioFigueiredo <julioo.1amorim@gmail.com>
2025-01-17 14:22:51 -0300

    feat: update code

---
+++ todolist/src/main/java/com/julio/todolist/user/UserController.java
@@ -0,0 +1,17 @@
+package com.julio.todolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    
+    // vem o corpo (body) da requisição
+    @PostMapping("/")
+    public void create(@RequestBody UserModel userModel){
+        System.out.println(userModel.getUsername());
+    }
+}
